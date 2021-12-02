8eac677585dfe5d8c9b2b97250e73fff99dc334c
Shellyroom <95006348+Shellyroom@users.noreply.github.com>
2021-12-02 11:25:51 +0900

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,10 @@
 // Image switcher code
 
 let myImage = document.querySelector('img');
+//documentは組み込みのオブジェクト。
+//ブラウザが表示しているページ。DOMとも呼ぶ。Document Object Modelの略。
+//query Selectorはページの一部（HTML要素）を選ぶ関数。
+
 
 myImage.onclick = function() {
   let mySrc = myImage.getAttribute('src');
@@ -18,11 +22,13 @@ let myHeading = document.querySelector('h1');
 
 function setUserName() {
   let myName = prompt('Please enter your name.');
+  //promptは組み込みの関数。Prompt(pop-up)Windowを表示して、ユーザ入力をさせる。
   if(!myName) {
     setUserName();
   } else {
     localStorage.setItem('name', myName);
-    myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    //入力データを、特別な場所（変数ではない、ファイルなどに相当）に保存する。
+    myHeading.innerHTML = 'Hello, ' + myName;
   }
 }
 
@@ -30,7 +36,7 @@ if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   let storedName = localStorage.getItem('name');
-  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+  myHeading.innerHTML = 'See you again, ' + storedName;
 }
 
 myButton.onclick = function() {
